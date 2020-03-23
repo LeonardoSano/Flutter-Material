ae96ff12c365452d38da0a00b6a14009b4d1b41d
leonardosano1995@gmail.com <leonardosano95@gmail.com>
2020-03-23 12:05:32 +0100

    aggiunto body con child e image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Material app'),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Material app'),
+          backgroundColor: Colors.blue[600],
+        ),
+        body: Center(
+          child: Image(
+              image:
+                NetworkImage('http://getwallpapers.com/wallpaper/full/2/e/a/176785.jpg'),
+          ),
+        ),
       ),
     ),
   );
